9c5635afc8e7d42ec17bb8ef9147208b5bb44da6
andreykoba <AKoba@ryazan.beeline.ru>
2025-02-06 19:03:11 +0300

    hkt and implicits

--- src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
+++ src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
@@ -1,5 +1,7 @@
 package ru.otus.module2
 
+import ru.otus.module2.higher_kinded_types.Bindable
+
 object homework_hkt_implicits{
 
   trait Bindable[F[_], A]{
@@ -7,5 +9,12 @@ object homework_hkt_implicits{
     def flatMap[B](f: A => F[B]): F[B]
   }
 
-  def tuplef[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
+  def tuplef[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
+    fa.flatMap(a => fb.map(b => (a, b)))
+
+  implicit def optBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+    override def map[B](f: A => B): Option[B] = opt.map(f)
+    override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+  }
+  //и тд для всех нужных нам тайп конструкторов
 }
