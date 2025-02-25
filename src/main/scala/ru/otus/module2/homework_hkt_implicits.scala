14ed60e3ad3a427cff9705f706b8744ec76cddcc
andreykoba <AKoba@ryazan.beeline.ru>
2025-02-25 15:43:01 +0300

    added Bindable2

--- src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
+++ src/main/scala/ru/otus/module2/homework_hkt_implicits.scala
@@ -20,4 +20,12 @@ object homework_hkt_implicits{
   def tuplef[F[_], A, B](fa: F[A], fb: F[B])(implicit convA: F[A] => Bindable[F, A], convB: F[B] => Bindable[F, B]): F[(A, B)] =
     convA(fa).flatMap(a => convB(fb).map(b => (a, b)))
 
+  // вариант 3
+  trait Bindable2[F[_]]{
+    def map[A, B](b: F[A])(f: A => B): F[B]
+    def flatMap[A, B](b: F[A])(f: A => F[B]): F[B]
+  }
+  def tuplef2[F[_], A, B](fa: F[A], fb: F[B])(implicit convA: Bindable2[F]): F[(A, B)] =
+    convA.flatMap(fa)(a => convA.map(fb)(b => (a, b)))
+
 }
